31873fda488423f25c9f398655376c32fb3cefe4
JeanBreves98 <JeanBreves1998@gmail.com>
2024-09-19 22:49:41 -0300

    Random Words

--- Part 7/2. Randomness/Random words.py
+++ Part 7/2. Randomness/Random words.py
@@ -2,27 +2,17 @@ from random import *
 
 def words(n: int, beginning: str):
     words = []
-    eligible = []
-    return_list = []
 
     with open("words.txt") as words_file:
         for line in words_file:
             line = line.replace("\n", "")
-            words.append(line)  # Appends each word in word list to a list of words
+            if line.startswith(beginning):
+                words.append(line)  # Appends each word in word list to a list of words
     
-    for word in words:
-        if word.startswith(beginning) == True:
-            eligible.append(word)
+    if len(words) < n:
+        raise ValueError("Not enough suitable words can be found!")
     
-    if len(eligible) < n:
-        raise ValueError
-    
-    for i in range(n):
-        selected = choice(eligible)
-        return_list.append(selected)
-        eligible.remove(selected)
-    
-    return return_list
+    return sample(words, n)
 
 
 if __name__ == "__main__":
